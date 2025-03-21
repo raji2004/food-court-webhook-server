ee033285792d452ffb453d744791320d0d397523
raji <kingraj1344@gmail.com>
2025-03-21 01:55:02 +0100

    fix: update Redis client URL to new endpoint

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 export const client = createClient({
-    url: "redis://default:IB4Ar175FzoPjRfuWsrjwAWY3qJYd09u@redis-11870.c283.us-east-1-4.ec2.redns.redis-cloud.com:11870"
+    url: "redis://default:IB4Ar175FzoPjRfuWsrjwAWY3qJYd09u@redis-13819.c10.us-east-1-2.ec2.redns.redis-cloud.com:13819"
 });
 
 client.on('error', err => console.log('Redis Client Error', err));
